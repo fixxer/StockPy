da0f7b945559bfa6536099a984e3b3ed504bfcda
Dmitry <ransom@hse.su>
2016-07-18 16:11:04 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+import csv
+
+
+#ah so
+
+if __name__ == '__main__':
+    print 'my god'
+    with open('test.csv', 'wb') as csvfile:
+        spamwriter = csv.writer(csvfile, delimiter=' ',
+                                quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        spamwriter.writerow(['Spam'] * 5 + ['Baked Beans'])
+        spamwriter.writerow(['Spam', 'Lovely Spam', 'Wonderful Spam'])
+
