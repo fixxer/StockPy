6b7b7329fbdba2e085d82c2e8b2392e73a31fb7b
Dmitry <ransom@hse.su>
2016-07-18 16:29:43 +0200

    add Stock class

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 import csv
 
+class Stock:
+    def __init__(self, ticker):
+        self.ticker = ticker  # instance variable unique to each instance
 
 #ah so
 
 if __name__ == '__main__':
-    print 'my god'
+
+    stock1 = Stock('AAPL')
+    stock2 = Stock('FB')
+
+    stocks = [stock1, stock2]
+
     with open('test.csv', 'wb') as csvfile:
-        spamwriter = csv.writer(csvfile, delimiter=' ',
-                                quotechar='|', quoting=csv.QUOTE_MINIMAL)
-        spamwriter.writerow(['Spam'] * 5 + ['Baked Beans'])
-        spamwriter.writerow(['Spam', 'Lovely Spam', 'Wonderful Spam'])
+        datawriter = csv.writer(csvfile, delimiter=' ', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        for s in stocks:
+            datawriter.writerow([s.ticker])
 
