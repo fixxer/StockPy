d9cdc775bf03e9508c449d6c346e862c2c3c15f6
Dmitry <ransom@hse.su>
2016-07-20 16:46:55 +0200

    first enrichment version

--- data_service.py
+++ data_service.py
@@ -1,21 +1,36 @@
 from bs4 import BeautifulSoup
 import requests
+from Stock import StockDescription
+
 
 def get_soup(base_url, *args):
-	url = base_url.format(*args)
-	html = requests.get(url).text
-	return BeautifulSoup(html, 'html5lib')
+    url = base_url.format(*args)
+    html = requests.get(url).text
+    return BeautifulSoup(html, 'html5lib')
+
 
 def company_profile(exchange, ticker):
-	soup = get_soup('http://financials.morningstar.com/cmpind/company-profile/component.action?component=BasicData&t={0}:{1}', exchange, ticker)
-	rows = soup.tbody.find_all('tr')
-	sector_and_industry = rows[5].find_all('td')
-	return {
-		'Sector': sector_and_industry[2].text,
-		'Industry': sector_and_industry[4].text,
-		'Style': rows[8].find_all('td')[0].text
-	}
+    soup = get_soup(
+        'http://financials.morningstar.com/cmpind/company-profile/component.action?component=BasicData&t={0}:{1}',
+        exchange, ticker)
+    rows = soup.tbody.find_all('tr')
+    sector_and_industry = rows[5].find_all('td')
+    return {
+        'Sector': sector_and_industry[2].text,
+        'Industry': sector_and_industry[4].text,
+        'Style': rows[8].find_all('td')[0].text
+    }
+
+
+def company_profile_stock(s):
+    soup = get_soup('http://financials.morningstar.com/cmpind/company-profile/component.action?component=BasicData&t={0}:{1}', s.exchange, s.ticker)
+    rows = soup.tbody.find_all('tr')
+    sector_and_industry = rows[5].find_all('td')
+    return {
+    'Sector': sector_and_industry[2].text,
+    'Industry': sector_and_industry[4].text,
+    'Style': rows[8].find_all('td')[0].text
+    }
 
 if __name__ == '__main__':
-	print company_profile('XNAS', 'FB')
-	
+    print company_profile('XNAS', 'FB')
--- main.py
+++ main.py
@@ -1,20 +1,33 @@
 import csv
-
-class Stock:
-    def __init__(self, ticker):
-        self.ticker = ticker  # instance variable unique to each instance
-
-#ah so
+from Stock import *
+from data_service import *
 
 if __name__ == '__main__':
 
-    stock1 = Stock('AAPL')
-    stock2 = Stock('FB')
+    #step 1. define portfolio
+    stock1 = StockDescription('AAPL', 'XNAS')
+    stock2 = StockDescription('FB', 'XNAS')
 
-    stocks = [stock1, stock2, Stock('ATVI')]
+    stocks = [stock1, stock2, StockDescription('ATVI','XNAS')]
 
-    with open('test.csv', 'wb') as csvfile:
+    # step2. load data
+    #for s in stocks:
+    #    print(s.ticker+': -> '+str(company_profile_stock(s)))
+
+    for s in stocks:
+        profile = company_profile_stock(s)
+        print(str(StockData(s, profile['Industry'], profile['Sector'], profile['Style'])))
+
+    # step3. calculate
+    #nothing to calculate
+
+    #step4. ???? create one object from different outputs??
+
+    #step5. generate output
+
+    with open('Stocks.csv', 'wb') as csvfile:
         datawriter = csv.writer(csvfile, delimiter=' ', quotechar='|', quoting=csv.QUOTE_MINIMAL)
         for s in stocks:
+            outputString = s.ticker + ';'
             datawriter.writerow([s.ticker])
 
