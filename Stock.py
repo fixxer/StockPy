b835afa72295a276de40764a2b317b677e5e9d39
fixxer <fixxer@inbox.ru>
2016-07-21 00:44:03 +0200

    Some refactoring
    1. Extracted company profile to class
    2. Leave singe company_profile func
    3. More descriptive __str__

--- Stock.py
+++ Stock.py
@@ -1,17 +1,24 @@
 class StockDescription:
     def __init__(self, ticker, exchange):
-        self.ticker = ticker  # instance variable unique to each instance
+        self.ticker = ticker
         self.exchange = exchange
 
     def __str__(self):
-        return "Ticker: {0}, Exchange: {1}".format(self.ticker, self.exchange)
+        return "Desc(Ticker={0}, Exchange={1})".format(self.ticker, self.exchange)
 
-class StockData:
-    def __init__(self, desc, sector, industry, style):
-        self.desc = desc
+class StockCompanyProfile:
+    def __init__(self, sector, industry, style):
         self.sector = sector
         self.industry = industry
         self.style = sector
 
     def __str__(self):
-        return "Desc: {0}; Sector: {1}, Industry: {2}, Style: {3}".format(self.desc, self.sector, self.industry, self.style)
+        return "Profile(Sector={0}, Industry={1}, Style={2})".format(self.sector, self.industry, self.style)
+
+class StockData:
+    def __init__(self, desc, profile):
+        self.desc = desc
+        self.profile = profile
+        
+    def __str__(self):
+        return "Stock(Desc={0}, Profile={1})".format(self.desc, self.profile)
--- data_service.py
+++ data_service.py
@@ -1,36 +1,19 @@
 from bs4 import BeautifulSoup
 import requests
-from Stock import StockDescription
-
+from Stock import *
 
 def get_soup(base_url, *args):
     url = base_url.format(*args)
     html = requests.get(url).text
     return BeautifulSoup(html, 'html5lib')
 
-
-def company_profile(exchange, ticker):
+def company_profile(stock_desc):
     soup = get_soup(
         'http://financials.morningstar.com/cmpind/company-profile/component.action?component=BasicData&t={0}:{1}',
-        exchange, ticker)
+        stock_desc.exchange, stock_desc.ticker)
     rows = soup.tbody.find_all('tr')
     sector_and_industry = rows[5].find_all('td')
-    return {
-        'Sector': sector_and_industry[2].text,
-        'Industry': sector_and_industry[4].text,
-        'Style': rows[8].find_all('td')[0].text
-    }
-
-
-def company_profile_stock(s):
-    soup = get_soup('http://financials.morningstar.com/cmpind/company-profile/component.action?component=BasicData&t={0}:{1}', s.exchange, s.ticker)
-    rows = soup.tbody.find_all('tr')
-    sector_and_industry = rows[5].find_all('td')
-    return {
-    'Sector': sector_and_industry[2].text,
-    'Industry': sector_and_industry[4].text,
-    'Style': rows[8].find_all('td')[0].text
-    }
-
-if __name__ == '__main__':
-    print company_profile('XNAS', 'FB')
+    sector = sector_and_industry[2].text
+    industry = sector_and_industry[4].text
+    style = rows[8].find_all('td')[0].text
+    return StockCompanyProfile(sector, industry, style)
