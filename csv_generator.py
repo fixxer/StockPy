0573697bf5f4acd73c4208bb6b42b0686035aa87
dkudishin <ransom@hse.su>
2016-07-21 21:12:22 +0200

    update csv_generator

--- csv_generator.py
+++ csv_generator.py
@@ -3,7 +3,6 @@ from stock import *
 
 def generate_csv(portfolio):
     with open('Stocks.csv', 'wb') as csvfile:
-        datawriter = csv.writer(csvfile, delimiter=' ', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        datawriter = csv.writer(csvfile)
         for s in portfolio:
-            #outputString = s.desc.ticker + ';'
             datawriter.writerow([s.ticker])
